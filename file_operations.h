a0f3bf73a9fb5a16305e2ba478f4de79a96f37b7
sunkarajayanth-stack <sunkarajayanth@klh.edu.in>
2026-09-23 19:26:18 +0530

    Create file_operations.h

---
+++ file_operations.h
@@ -0,0 +1,10 @@
+#ifndef FILE_OPERATIONS_H
+#define FILE_OPERATIONS_H
+
+void file_information(void);
+void read_file(void);
+void create_file(void);
+void delete_file(void);
+void rename_item(void);
+
+#endif
