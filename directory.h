fedc471bb40dfd61bd5021f8caeae5a9957c090a
sunkarajayanth-stack <sunkarajayanth@klh.edu.in>
2026-09-23 19:25:36 +0530

    Create directory.h

---
+++ directory.h
@@ -0,0 +1,10 @@
+#ifndef DIRECTORY_H
+#define DIRECTORY_H
+
+void list_directory(void);
+void change_directory(void);
+void parent_directory(void);
+void create_directory(void);
+void delete_directory(void);
+
+#endif
